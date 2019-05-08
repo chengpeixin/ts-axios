2a0d0cdadaa9c11b978f1a0b577ca451701d8986
xinpeicheng <xinpeicheng@yeah.net>
2019-05-08 10:08:52 +0800

    feat(helpers/url.ts helpers/util.ts index.ts xhr.ts): no
    get请求测试及get请求参数过滤

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import { AxiosRequestConfig } from './types'
-function axios(config: AxiosRequestConfig) {}
+import xhr from './xhr'
+import { buildURL } from './helpers/url'
+function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
+  return xhr(config)
+}
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
 export default axios
