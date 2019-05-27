e99ff6e0ae0c486e90a451c68699f64b3689495e
xinpeicheng <xinpeicheng@yeah.net>
2019-05-27 22:45:32 +0800

    feat(增加拦截器代码，增加拦截器ts类型): no
    no

--- src/axios.ts
+++ src/axios.ts
@@ -4,20 +4,10 @@ import { extend } from './helpers/util'
 function createInstance(): AxiosInstance {
   const context = new Axios()
   const instance = Axios.prototype.request.bind(context)
-  console.log(
-    '------------------------------------------------------------------------------------'
-  )
-  console.log([instance])
-  console.log(context)
-  console.log(
-    '------------------------------------------------------------------------------------'
-  )
   extend(instance, context)
   return instance as AxiosInstance
 }
 
 const axios = createInstance()
-console.log([axios])
-// console.log(axios)
 
 export default axios
